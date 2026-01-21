411b1e86cc71df7aada67db795c8a2ee260d9cde
Dmitriy Gulenkov <gulenkov.dmitriy234@gmail.com>
2026-01-21 11:52:46 +0300

    add animation(transition)

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,67 @@
 <template>
-	<div class="max-w-4xl mx-auto px-4 py-12 sm:px-6 lg:px-8">
-		<div class="img-block rounded-xl overflow-hidden mb-8">
-			<img class="object-cover" src="./assets/img/myphoto.webp" alt="">
-		</div>
-		<div class="flex text-center flex-col gap-1 mb-6">
-			<h1 class="title text-xl font-semibold">Frontend pet project</h1>
-			<h2 class="subtitle text-lg text-slate-500">Мои пет-проекты </h2>
-		</div>
-		<ProjectList/>
-	</div>
+	<transition v-if="loading" name="fade">
+		<Preloader />
+	</transition>
+	<transition v-else name="fade-slide" appear>
+			<div class="max-w-4xl mx-auto px-4 py-12 sm:px-6 lg:px-8">
+			<div class="img-block rounded-xl overflow-hidden mb-8">
+				<img class="object-cover" src="./assets/img/myphoto.webp" alt="">
+			</div>
+			<div class="flex text-center flex-col gap-1 mb-6">
+				<h1 class="title text-xl font-semibold">Frontend pet project</h1>
+				<h2 class="subtitle text-lg text-slate-500">Мои пет-проекты</h2>
+			</div>
+			<ProjectList/>
+			</div>
+	</transition>
 </template>
 
 <script>
+import Preloader from './components/Preloader.vue';
 import ProjectList from './components/ProjectList.vue';
 
-	export default {
-		components: {
-			ProjectList,
+export default {
+	components: {
+		ProjectList,
+		Preloader
+	},
+	data(){
+		return{
+			loading: true,
+		};
+	},
+	methods:{
+		startLoading() {
+			setTimeout(()=>{
+				this.loading = false
+			}, 2000)
 		}
+	},
+	mounted(){
+		this.startLoading()
 	}
+}
+
 </script>
 
 <style lang="scss">
+.fade-enter-active, .fade-leave-active {
+	transition: opacity 0.5s;
+}
+.fade-enter-from, .fade-leave-to {
+	opacity: 0;
+}
 
+.fade-slide-enter-active {
+	transition: opacity 0.8s ease, transform 0.8s ease;
+}
+.fade-slide-enter-from {
+	opacity: 0;
+	transform: translateY(20px);
+}
+.fade-slide-enter-to {
+	opacity: 1;
+	transform: translateY(0);
+}
 </style>
+
--- src/components/ProjectItem.vue
+++ src/components/ProjectItem.vue
@@ -3,13 +3,13 @@
 		<h4 class="title__project font-medium">{{ title }}</h4>
 		<div class="description__project text-sm text-slate-500">{{ description }}</div>
 		<div class="flex gap-2 text-sm text-slate-700">
-			<a class="flex items-center gap-1" :href="rephref">				
-				<SvgIcon class="w-4 h-4 def-icon stroke-slate-500" icon="#git"/>
-				<span>Репозиторий</span> 
-			</a>
-			<a class="flex items-center gap-1" :href="sitehref">				
+			<a class="flex items-center gap-1" :href="sitehref" target="_blank">				
 				<SvgIcon class="w-4 h-4 def-icon stroke-slate-500" icon="#link"/>
-				<span>Сайт</span> 
+				<span>Site</span> 
+			</a>
+			<a class="flex items-center gap-1" :href="rephref" target="_blank">				
+				<SvgIcon class="w-4 h-4 def-icon stroke-slate-500" icon="#git"/>
+				<span>GitHub</span> 
 			</a>
 		</div>
 	</div>
